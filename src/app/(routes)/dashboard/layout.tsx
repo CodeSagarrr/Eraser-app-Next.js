94343b0766c8972ab4769185ccf497727fe45fb8
Sagar Vishwakarma <sv472921@gmail.com>
2024-12-01 15:58:54 +0530

    Initial commit

--- src/app/(routes)/dashboard/layout.tsx
+++ src/app/(routes)/dashboard/layout.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 import { useConvex } from 'convex/react';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { api } from '../../../../convex/_generated/api';
 import { useRouter } from 'next/navigation';
 import SideBar from './_components/SideBar';
@@ -12,8 +12,8 @@ function layout({ children }: Readonly<{
     children: React.ReactNode;
 }>) {
 
-    const { user }: any = useKindeBrowserClient();
-    const [ _FileList , setFileList] = useState<Team[]>()
+    const { user }:any = useKindeBrowserClient();
+    const [ _FileList , setFileList] = useState<Team[]>();
     const convex = useConvex();
     const navigate = useRouter();
 
@@ -21,12 +21,12 @@ function layout({ children }: Readonly<{
         const res = await convex.query(api.Teams.getTeams, { email: user?.email });
         if (!res?.length) {
             navigate.push("/teams/create")
-        }
+        };
     }
 
     useEffect(() => {
         user && getTeams();
-    }, [user])
+    }, [user]);
 
     return (
         <FileListContext.Provider value={{_FileList , setFileList}}>
